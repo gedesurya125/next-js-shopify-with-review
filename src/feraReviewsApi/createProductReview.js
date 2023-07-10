d51bedd816f1ffeded702ad40326221ca9031af5
gedesurya125 <gedesurya125@gmail.com>
2023-07-10 17:01:52 +0800

    add create review api

--- src/feraReviewsApi/createProductReview.js
+++ src/feraReviewsApi/createProductReview.js
@@ -5,27 +5,13 @@ export const createProductReview = async ({
   customerName,
   customerEmail,
 }) => {
-  // const formData = new FormData();
-
-  // formData.append("heading", heading);
-  // formData.append("body", body);
-  // formData.append("external_product_id", productId);
-  // formData.append(
-  //   "customer",
-  //   JSON.stringify({
-  //     name: customerName,
-  //     email: customerEmail,
-  //   })
-  // );
-  // formData.append("rating", "4"), formData.append("state", "approved");
-
   const options = {
     method: "POST",
     headers: {
       accept: "application/json",
       "content-type": "application/json",
       // mode: "no-cors",
-      "SECRET-KEY": process.env.NEXT_PUBLIC_FERA_SECRET_KEY,
+      // "SECRET-KEY": process.env.NEXT_PUBLIC_FERA_SECRET_KEY,
     },
     body: JSON.stringify({
       data: {
@@ -45,11 +31,13 @@ export const createProductReview = async ({
 
   const response = await fetch(
     // `https://api.fera.ai/v3/private/reviews?api_key=${process.env.NEXT_PUBLIC_FERA_SECRET_KEY}&secret_key=${process.env.NEXT_PUBLIC_FERA_SECRET_KEY}`,
-    `https://api.fera.ai/v3/private/reviews/`,
+    // `https://api.fera.ai/v3/private/reviews/`,
+    "/api/create_shopify_review",
     options
   )
     .then((response) => response.json())
     .catch((err) => console.error(err));
 
+  console.log("this is the response", response);
   return response;
 };
---
+++ src/pages/api/create_shopify_review.js
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function handler(req, res) {
+  console.log("this is the body", req?.body?.data);
+
+  if (!req?.body?.data) return res.status(400);
+
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      "SECRET-KEY": process.env.NEXT_PUBLIC_FERA_SECRET_KEY,
+    },
+    body: JSON.stringify(req.body.data),
+  };
+
+  const response = await fetch(
+    "https://api.fera.ai/v3/private/reviews",
+    options
+  )
+    .then((response) => response.json())
+    .catch((err) => console.error(err));
+
+  res
+    .status(200)
+    .json({ message: "success", data: response, payload: req.body.data });
+}
