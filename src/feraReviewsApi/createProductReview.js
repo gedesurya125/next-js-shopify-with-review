01ce248c40f21e37162c2c7b469822de47079c67
gedesurya125 <gedesurya125@gmail.com>
2023-07-10 15:44:22 +0800

    try to put in the correct  body data

--- src/feraReviewsApi/createProductReview.js
+++ src/feraReviewsApi/createProductReview.js
@@ -5,32 +5,47 @@ export const createProductReview = async ({
   customerName,
   customerEmail,
 }) => {
-  const formData = new FormData();
+  // const formData = new FormData();
 
-  formData.append("heading", heading);
-  formData.append("body", body);
-  formData.append("external_product_id", productId);
-  formData.append(
-    "customer",
-    JSON.stringify({
-      name: customerName,
-      email: customerEmail,
-    })
-  );
+  // formData.append("heading", heading);
+  // formData.append("body", body);
+  // formData.append("external_product_id", productId);
+  // formData.append(
+  //   "customer",
+  //   JSON.stringify({
+  //     name: customerName,
+  //     email: customerEmail,
+  //   })
+  // );
+  // formData.append("rating", "4"), formData.append("state", "approved");
 
   const options = {
     method: "POST",
     headers: {
-      accept: "multipart/form-date",
-      "content-type": "multipart/form-data",
-      // "SECRET-KEY": process.env.NEXT_PUBLIC_FERA_SECRET_KEY,
+      accept: "application/json",
+      "content-type": "application/json",
+      // mode: "no-cors",
+      "SECRET-KEY": process.env.NEXT_PUBLIC_FERA_SECRET_KEY,
     },
-    body: formData,
+    body: JSON.stringify({
+      data: {
+        heading,
+        body: JSON.stringify(body),
+        external_product_id: productId,
+        customer: {
+          name: customerName,
+          email: customerEmail,
+        },
+        rating: 4,
+        state: "approved",
+      },
+    }),
+    // body: formData,
   };
 
   const response = await fetch(
-    `https://api.fera.ai/v3/private/reviews?secret_key=${process.env.NEXT_PUBLIC_FERA_SECRET_KEY}`,
-    // `https://api.fera.ai/v3/private/reviews`,
+    // `https://api.fera.ai/v3/private/reviews?api_key=${process.env.NEXT_PUBLIC_FERA_SECRET_KEY}&secret_key=${process.env.NEXT_PUBLIC_FERA_SECRET_KEY}`,
+    `https://api.fera.ai/v3/private/reviews/`,
     options
   )
     .then((response) => response.json())
